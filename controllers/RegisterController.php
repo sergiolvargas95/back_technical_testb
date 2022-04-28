c7604c0624612cca7e9924d6f850f9084ee05535
Diego Herrera <daherre0@gmail.com>
2022-04-28 16:39:43 -0500

    .class deprecated

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -22,7 +22,7 @@ class RegisterController extends Controller
             parent::behaviors(),
             [
                 'verbs' => [
-                    'class' => VerbFilter::className(),
+                    'class' => VerbFilter::class,
                     'actions' => [
                         'delete' => ['POST'],
                     ],
